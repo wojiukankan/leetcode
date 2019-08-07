9de5ea3d5f21d3035bbfa8073941a9f2b6c532f2
853130802 <853130802@qq.com>
2019-08-07 15:03:52 +0800

    6

--- 5.php
+++ 5.php
@@ -1,28 +1,48 @@
 <?php
 
 class Solution {
-
     /**
      * @param String $s
-     * @param Integer $numRows
      * @return String
      */
-    function convert($s, $numRows) {
-        $k = $i = 0;
-        $arr = [];
-        $diff = 1;
+    function longestPalindrome($s) {
+
+        $separator = '#';
+        $i = 0;
+        $str = $separator;
         while (isset($s[$i])) {
-            $arr[$k] = isset($arr[$k]) ? $arr[$k] . $s[$i] : $s[$i];
+            $str = $str . $s[$i] . $separator;
             $i++;
-            if ($k == 0) {
-                $diff = 1;
-            } elseif ($k == $numRows - 1) {
-                $diff = -1;
-            }
-            $k += $diff;
         }
-        return implode('', $arr);
+        $i = 0;
+        $mx = 0;
+        $p = [];
+        $max = 0;
+        $center = 0;
+        $id = 0;
+        while (isset($str[$i])) {
+            if ($i < $mx) {
+                $p[$i] = min($p[2 * $id - $i], $mx - $i);
+            } else {
+                $p[$i] = 1;
+            }
+            while ($i - $p[$i] >= 0 && isset($str[$i + $p[$i]]) && $str[$i - $p[$i]] == $str[$i + $p[$i]]) {
+                $p[$i]++;
+            }
+
+            if ($mx < $i + $p[$i]) {
+                $id = $i;
+                $mx = $i + $p[$i];
+            }
+            if ($max < $p[$i] - 1) {
+                $max = $p[$i] - 1;
+                $center = $i;
+            }
+            $i++;
+        }
+        return substr($s, ($center - $max) / 2, $max);
     }
 }
 
-var_dump((new Solution)->convert('asfasqweqrqw', 3));
+
+var_dump((new Solution)->longestPalindrome("aaa"));
