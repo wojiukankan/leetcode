06a513d86f169ba68dc912ab52abd49df9115731
853130802 <853130802@qq.com>
2019-08-12 12:05:32 +0800

    2.go

---
+++ 11.php
@@ -0,0 +1,14 @@
+<?php
+
+class Solution {
+
+    /**
+     * @param Integer[] $height
+     * @return Integer
+     */
+    function maxArea($height) {
+
+    }
+}
+
+var_dump((new Solution)->maxArea([2, 7, 11, 15]));
--- 2/2.go
+++ 2/2.go
@@ -2,6 +2,48 @@ package main
 
 import "fmt"
 
+type ListNode struct {
+	Val  int
+	Next *ListNode
+}
+
 func main() {
-	fmt.Print()
+	l1 := &ListNode{1, nil}
+	l2 := &ListNode{9, &ListNode{9, nil}}
+	a := addTwoNumbers(l1, l2)
+	fmt.Print(a)
+}
+
+func addTwoNumbers(l1 *ListNode, l2 *ListNode) *ListNode {
+	var new, next *ListNode
+	carry := 0
+	for l1 != nil || l2 != nil {
+		tmp := 0
+		tmp = tmp + carry
+		carry = 0
+		if l1 != nil {
+			tmp = tmp + l1.Val
+			l1 = l1.Next
+		}
+		if l2 != nil {
+			tmp = tmp + l2.Val
+			l2 = l2.Next
+		}
+		if tmp >= 10 {
+			tmp = tmp - 10
+			carry = 1
+		}
+		if next == nil {
+			next = &ListNode{tmp, nil}
+			new = next
+		} else {
+			next.Next = &ListNode{tmp, nil}
+			next = next.Next
+		}
+
+	}
+	if carry == 1 {
+		next.Next = &ListNode{1, nil}
+	}
+	return new
 }
