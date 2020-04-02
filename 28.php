c564623bf9e8e5f498fe49ffbd3c1e213d6f7a87
853130802 <853130802@qq.com>
2020-04-02 11:53:52 +0800

    28

--- 28.php
+++ 28.php
@@ -1,36 +1,30 @@
 <?php
 
-class Solution {
-
+class Solution
+{
     /**
      * @param String $haystack
      * @param String $needle
      * @return Integer
      */
-    function strStr($haystack, $needle) {
+    function strStr($haystack, $needle)
+    {
         if ($needle === '') {
             return 0;
         }
+        $lenH = strlen($haystack);//10
+        $lenN = strlen($needle);//5
         $i = 0;
-        $j = 0;
-        while (isset($haystack[$i])) {
-            if (!isset($needle[0 + $j])) {
-                return $i - $j;
-            }
-            if ($haystack[$i] == $needle[0 + $j]) {
-                $j++;
-            } else {
-                $j = 0;
+        while ($i <= $lenH - $lenN) {
+            if ($haystack[$i] === $needle[0] && substr($haystack, $i, $lenN) === $needle) {
+                return $i;
             }
             $i++;
-        }
-        if (!isset($needle[0 + $j])) {
-            return $i - $j;
         }
         return -1;
     }
 }
 
-$haystack = 'mississippi';
-$needle = 'issip';
+$haystack = 'hello';
+$needle = 'll';
 var_dump((new Solution)->strStr($haystack, $needle));
